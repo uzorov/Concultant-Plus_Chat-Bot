34e219def6a9abb60dcbdab60dfbca88e1c4e477
Your Name <e@w.com>
2022-09-29 16:29:29 +0300

    Добавлены два экрана с графическим интерфейсом

---
+++ app/src/main/java/exportkit/figma/android_large___1_activity.java
@@ -0,0 +1,56 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___1
+	 *	@date 		Wednesday 28th of September 2022 08:57:46 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class android_large___1_activity extends Activity {
+
+	
+	private View _bg__android_large___1_ek2;
+	private ImageView konsul_tantplyus_1;
+	private View ellipse_1;
+	private View container;
+	private TextView ________________;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___1);
+
+		
+		_bg__android_large___1_ek2 = (View) findViewById(R.id._bg__android_large___1_ek2);
+		konsul_tantplyus_1 = (ImageView) findViewById(R.id.konsul_tantplyus_1);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		container = (View) findViewById(R.id.container);
+		________________ = (TextView) findViewById(R.id.________________);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/main/java/exportkit/figma/android_large___2_activity.java
@@ -0,0 +1,112 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___1
+	 *	@date 		Wednesday 28th of September 2022 08:57:46 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class android_large___2_activity extends Activity {
+
+	
+	private View _bg__android_large___2_ek2;
+	private View rectangle_2;
+	private TextView __;
+	private TextView __________;
+	private TextView ___________;
+	private TextView _15_18_pm;
+	private TextView _15_20_pm;
+	private TextView ________27_________;
+	private View rectangle_3;
+	private View rectangle_13;
+	private View rectangle_6;
+	private View rectangle_7;
+	private TextView ___________________;
+	private TextView ____________________ek1;
+	private TextView _______________;
+	private TextView _______________________;
+	private View rectangle_3_ek1;
+	private TextView _________________________________;
+	private View rectangle_4;
+	private TextView _6893849523834;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View rectangle_10;
+	private TextView __________________________;
+	private TextView ________________________;
+	private TextView _____________;
+	private View rectangle_14;
+	private View rectangle_15;
+	private View rectangle_16;
+	private TextView _15_20_pm_ek1;
+	private View ellipse_2;
+	private ImageView ______________1;
+	private View ellipse_1_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___2);
+
+		
+		_bg__android_large___2_ek2 = (View) findViewById(R.id._bg__android_large___2_ek2);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		__ = (TextView) findViewById(R.id.__);
+		__________ = (TextView) findViewById(R.id.__________);
+		___________ = (TextView) findViewById(R.id.___________);
+		_15_18_pm = (TextView) findViewById(R.id._15_18_pm);
+		_15_20_pm = (TextView) findViewById(R.id._15_20_pm);
+		________27_________ = (TextView) findViewById(R.id.________27_________);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		rectangle_13 = (View) findViewById(R.id.rectangle_13);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		rectangle_7 = (View) findViewById(R.id.rectangle_7);
+		___________________ = (TextView) findViewById(R.id.___________________);
+		____________________ek1 = (TextView) findViewById(R.id.____________________ek1);
+		_______________ = (TextView) findViewById(R.id._______________);
+		_______________________ = (TextView) findViewById(R.id._______________________);
+		rectangle_3_ek1 = (View) findViewById(R.id.rectangle_3_ek1);
+		_________________________________ = (TextView) findViewById(R.id._________________________________);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		_6893849523834 = (TextView) findViewById(R.id._6893849523834);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		rectangle_10 = (View) findViewById(R.id.rectangle_10);
+		__________________________ = (TextView) findViewById(R.id.__________________________);
+		________________________ = (TextView) findViewById(R.id.________________________);
+		_____________ = (TextView) findViewById(R.id._____________);
+		rectangle_14 = (View) findViewById(R.id.rectangle_14);
+		rectangle_15 = (View) findViewById(R.id.rectangle_15);
+		rectangle_16 = (View) findViewById(R.id.rectangle_16);
+		_15_20_pm_ek1 = (TextView) findViewById(R.id._15_20_pm_ek1);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		______________1 = (ImageView) findViewById(R.id.______________1);
+		ellipse_1_ek1 = (View) findViewById(R.id.ellipse_1_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
