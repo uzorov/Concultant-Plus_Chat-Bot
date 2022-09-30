9da275f5aeeb4f101b2fd0ebf1cd5422d16c287f
Your Name <e@w.com>
2022-09-30 15:09:09 +0300

    Исправлены ошибки сборки проекта

---
+++ app/src/main/java/exportkit/figma/ChattingActivity.java
@@ -0,0 +1,81 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___1
+	 *	@date 		Wednesday 28th of September 2022 08:57:46 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class ChattingActivity extends Activity {
+
+	
+	private View _bg__android_large___2_ek2;
+	private View rectangle_2;
+	private TextView __;
+	private TextView __________;
+	private TextView ___________;
+	private TextView _15_18_pm;
+	private TextView _15_20_pm;
+	private TextView ________27_________;
+	private View rectangle_3;
+	private View rectangle_13;
+	private View rectangle_6;
+	private View rectangle_7;
+	private TextView ___________________;
+	private TextView ____________________ek1;
+	private TextView _______________;
+	private TextView _______________________;
+	private View rectangle_3_ek1;
+	private TextView _________________________________;
+	private View rectangle_4;
+	private TextView _6893849523834;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View rectangle_10;
+	private TextView __________________________;
+	private TextView ________________________;
+	private TextView _____________;
+	private View rectangle_14;
+	private View rectangle_15;
+	private View rectangle_16;
+	private TextView _15_20_pm_ek1;
+	private View ellipse_2;
+	private ImageView ______________1;
+	private View ellipse_1_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___2);
+
+		
+		_bg__android_large___2_ek2 = (View) findViewById(R.id._bg__android_large___2_ek2);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
--- app/src/main/java/exportkit/figma/InvitToTheChatActivity.java
+++ app/src/main/java/exportkit/figma/InvitToTheChatActivity.java
@@ -25,7 +25,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class android_large___1_activity extends Activity {
+public class InvitToTheChatActivity extends Activity {
 
 	
 	private View _bg__android_large___1_ek2;
@@ -40,12 +40,10 @@ public class android_large___1_activity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.android_large___1);
 
-		
-		_bg__android_large___1_ek2 = (View) findViewById(R.id._bg__android_large___1_ek2);
 		konsul_tantplyus_1 = (ImageView) findViewById(R.id.konsul_tantplyus_1);
 		ellipse_1 = (View) findViewById(R.id.ellipse_1);
 		container = (View) findViewById(R.id.container);
-		________________ = (TextView) findViewById(R.id.________________);
+
 	
 		
 		//custom code goes here
