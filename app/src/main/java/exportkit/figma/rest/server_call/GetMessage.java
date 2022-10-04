10cd57341a7ac847b4d3c83be3b2304b2cc0d89a
Arseniy Solonets <solosen@bk.ru>
2022-10-04 18:12:22 +0300

    in GetMessage class added ability to call overloaded method without parameters

--- app/src/main/java/exportkit/figma/rest/server_call/GetMessage.java
+++ app/src/main/java/exportkit/figma/rest/server_call/GetMessage.java
@@ -15,8 +15,7 @@ import retrofit2.Response;
  * для этого необходимо вызвать
  * GetMessage.getMessage();
  * ответ - текущее сообщение в виде объекта NodeModel.
- * Для первого вызова необходимо передать параметры:
- * ("старт", "")
+ * Для первого вызова необходимо использовать перегруженный метод без параметров.
  */
 public class GetMessage {
 
@@ -29,6 +28,37 @@ public class GetMessage {
                 .getRetrofit()
                 .create(NodeModelAPI.class)
                 .getNode(clientsAnswer, previousQuestion);
+
+        call.enqueue(new Callback<NodeModel>() {
+            @Override
+            public void onResponse(Call<NodeModel> call, Response<NodeModel> response) {
+                if (response.isSuccessful()) {
+                    NodeModel nodeModel = response.body();
+                    nodeModelToReturn[0] = nodeModel;
+                    Log.d("[INFO]:", nodeModel.toString());
+                } else {
+                    Log.d("[Error]:", "Ошибка ответа сервера");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NodeModel> call, Throwable t) {
+                Log.d("[Error]:", "Ошибка обращения к серверу");
+            }
+        });
+        return nodeModelToReturn[0];
+    }
+
+
+    public static NodeModel getMessage() {
+        final NodeModel[] nodeModelToReturn = new NodeModel[1];
+        nodeModelToReturn[0] = new NodeModel();
+
+        RetrofitService retrofitService = new RetrofitService();
+        Call<NodeModel> call = retrofitService
+                .getRetrofit()
+                .create(NodeModelAPI.class)
+                .getNode("старт", "");
 
         call.enqueue(new Callback<NodeModel>() {
             @Override
